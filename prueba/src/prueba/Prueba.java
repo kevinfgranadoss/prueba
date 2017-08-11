369a1a67d589232325e487bb9b52e6f0b03bfec1
Estudiantes <Estudiantes@labing50422.udistrital.edu.co>
2017-08-11 16:13:19 -0500

    se adiciona la funcionalidad de imprimir

--- prueba/src/prueba/Prueba.java
+++ prueba/src/prueba/Prueba.java
@@ -16,6 +16,7 @@ public class Prueba {
      */
     public static void main(String[] args) {
         System.out.println("Hola mundo");
+        //
         // TODO code application logic here
     }
 
