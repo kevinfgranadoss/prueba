13bd50a94bdd83e9de84f31ef53f2a24ccbea998
Estudiantes <Estudiantes@labing50422.udistrital.edu.co>
2017-08-11 16:23:34 -0500

    se imprime 100 veces "hola mundo"

--- prueba/src/prueba/Prueba.java
+++ prueba/src/prueba/Prueba.java
@@ -15,9 +15,11 @@ public class Prueba {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola mundo");
-        //
-        // TODO code application logic here
+        int i;
+        for (i = 1; i < 100; i++) {
+            System.out.println("Hola mundo");
+            //
+            // TODO code application logic here
+        }
     }
-
 }
