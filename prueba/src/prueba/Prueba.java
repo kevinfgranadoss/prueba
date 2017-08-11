a5d7be5e86905ae9de2038b3fa8e62c333e77391
Estudiantes <Estudiantes@labing50422.udistrital.edu.co>
2017-08-11 16:12:22 -0500

    clase de ejemplazo

--- prueba/src/prueba/Prueba.java
+++ prueba/src/prueba/Prueba.java
@@ -15,7 +15,8 @@ public class Prueba {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("Hola mundo");
         // TODO code application logic here
     }
-    
+
 }
